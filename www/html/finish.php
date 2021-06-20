ec0cf4b0b94e44594acceca2623350a030c2d06a
Mizuta Kokoro <mizutakokoro@ne.jp>
2021-06-20 12:28:42 +0900

    テーブル追加のデータ保存

--- www/html/finish.php
+++ www/html/finish.php
@@ -30,4 +30,18 @@ if(purchase_carts($db, $carts) === false){
 
 $total_price = sum_carts($carts);
 
+$db -> beginTransaction(); //トランザクション開始
+
+$insert_buy = insert_buy($db,$carts[0]['user_id'],$total_price);//user_id $total_price(合計金額) 購入履歴テーブルの追加
+$buy_id = $db ->lastInsertId(); //最後に挿入された行のIDを取得する
+$insert_details = details_insert($db, $buy_id, $carts);//$cartsを使ってitem_idと商品価格と個数を購入明細テーブルに追加
+
+if($insert_buy===true && $insert_details===true){
+  $db -> commit(); //両方ともtrueの時はコミット
+
+}else{
+  $db -> rollback(); //ロールバック
+  redirect_to(CART_URL);
+}
+
 include_once '../view/finish_view.php';
